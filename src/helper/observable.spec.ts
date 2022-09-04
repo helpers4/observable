020e4d6ba04bef9fd1f3058088f849d3eeb85400
Bérenger <berenger@arnaud.xyz>
2022-09-04 23:21:09 +0200

    test: :white_check_mark: add missing test for combine -> pre-treament

--- src/helper/observable.spec.ts
+++ src/helper/observable.spec.ts
@@ -3,7 +3,7 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import test from 'ava';
-import { firstValueFrom, of } from 'rxjs';
+import { firstValueFrom, map, of } from 'rxjs';
 
 import { combine } from './observable';
 
@@ -12,4 +12,13 @@ test('combine', async (t) => {
     combine(of(1), of(2), (c) => c[0] + c[1])
   );
   return t.is(result, 3);
+});
+
+test('combine with pretreatment', async (t) => {
+  const result = await firstValueFrom(
+    combine(of(1), of(2), (c) => c[0] + c[1], {
+      preTreatment: map((c) => [c[0], c[1] + 1]),
+    })
+  );
+  return t.is(result, 4);
 });
