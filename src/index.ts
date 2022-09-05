072f3150eb73a1eecbaeeb3bf207f25fa94d119e
Bérenger <berenger@arnaud.xyz>
2022-09-05 23:31:02 +0200

    chore: :memo: fix doc namespace

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+/*
+ * This program is under the terms of the GNU Affero General Public License version 3
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+/**
+ * Library of observable helpers.
+
+ * @packageDocumentation
+ * @module observable
+ * @preferred
+ */
+
 export * from './helper/observable';
